27227c685073a36837e4952a12d674d198bf730b
Michael Jones <m.pricejones@gmail.com>
2015-05-05 17:59:13 +0100

    Parsing the lines correctly
    I'm flailing around here so happy to commit whenever something goes my
    way.

--- pact.py
+++ pact.py
@@ -5,6 +5,7 @@ import re
 
 from pypeg2 import parse, List, word, name, endl, restline, maybe_some, optional, some
 
+whitespace = re.compile(r'\s+')
 
 class Import(List):
     grammar = 'import', name()
@@ -12,18 +13,39 @@ class Import(List):
 class Function(List):
     grammar = 'def', some(re.compile('(?s).+'))
 
+
+class NonPactLine(List):
+    grammar = re.compile('.+'), '\n'
+
+
+class Attribute(object):
+    grammar = name(), '=', '{twitter_share}'
+
+
+class Attributes(List):
+    grammar = maybe_some(Attribute)
+
+
+class Tag(List):
+    grammar = '<', name(), whitespace, Attributes, '>'
+
+
+class PactBlock(List):
+    grammar = re.compile(r'[^<\n]+'), Tag
+
+
 class File(List):
     grammar = maybe_some([
         '\n',
-        Function,
-        re.compile('(?s).+')
+        PactBlock,
+        NonPactLine,
     ])
 
 
 text = u"""
 def tag(self):
     twitter_share = ""
-    return <a href="{twitter_share}"><i class="fa fa-twitter-square large-icon"></i></a>
+    return <a href={twitter_share}><i class="fa fa-twitter-square large-icon"></i></a>
 """
 
 import pdb
